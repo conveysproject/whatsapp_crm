42e0b107a9782bb0557b0938817afee5110870cb
TrustCRM <conveysproject@gmail.com>
2026-04-28 13:30:02 +0530

    feat(api): add Socket.io server plugin with org-scoped rooms

---
+++ apps/api/src/plugins/socketio.ts
@@ -0,0 +1,33 @@
+import fp from "fastify-plugin";
+import { Server as SocketIOServer } from "socket.io";
+import type { FastifyInstance } from "fastify";
+import { setIo } from "../lib/io-ref.js";
+
+declare module "fastify" {
+  interface FastifyInstance {
+    io: SocketIOServer;
+  }
+}
+
+export default fp(async (fastify: FastifyInstance) => {
+  const io = new SocketIOServer(fastify.server, {
+    cors: {
+      origin: process.env["NEXT_PUBLIC_APP_URL"] ?? "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  setIo(io);
+
+  io.on("connection", (socket) => {
+    socket.on("join-org", (organizationId: string) => {
+      void socket.join(`org:${organizationId}`);
+    });
+  });
+
+  fastify.decorate("io", io);
+
+  fastify.addHook("onClose", async () => {
+    await io.close();
+  });
+});
