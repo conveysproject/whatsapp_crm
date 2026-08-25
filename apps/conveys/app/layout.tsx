c881f7c9a6546bdaf88772a8caffd8cd540d4cb6
TrustCRM <conveysproject@gmail.com>
2026-08-25 20:28:38 +0530

    Guard Tawk loader in both layouts

--- apps/conveys/app/layout.tsx
+++ apps/conveys/app/layout.tsx
@@ -136,15 +136,19 @@ export default function RootLayout({ children }: { children: ReactNode }): JSX.E
       {/* Tawk.to Live Chat */}
       <Script id="tawk-to" strategy="afterInteractive">
         {`
-          var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-          (function () {
-            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = "https://embed.tawk.to/6a8d9f787f08c0344498a708/1k0sjdsk2";
-            s1.charset = "UTF-8";
-            s1.setAttribute("crossorigin", "*");
-            s0.parentNode.insertBefore(s1, s0);
-          })();
+          window.Tawk_API = window.Tawk_API || {};
+          if (!window.__tawkLoaded) {
+            window.__tawkLoaded = true;
+            window.Tawk_LoadStart = new Date();
+            (function () {
+              var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+              s1.async = true;
+              s1.src = "https://embed.tawk.to/6a8d9f787f08c0344498a708/1k0sjdsk2";
+              s1.charset = "UTF-8";
+              s1.setAttribute("crossorigin", "*");
+              s0.parentNode.insertBefore(s1, s0);
+            })();
+          }
         `}
       </Script>
 
